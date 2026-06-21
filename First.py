1e4178aef7a258912c9b754125d006492c827d17
nirdeshlc-droid <nirdeshlc@gmail.com>
2026-06-21 08:48:40 +0545

    Python

---
+++ First.py
@@ -0,0 +1,31 @@
+# a=int(input("enter first number= "));
+# b=int(input("enter second number= "));
+# c = a + b;
+# print("sum is",c);
+
+
+# x=int(input("enter your age= "));
+# if x>=18:
+#     print("you r eligible to vote");
+# else :
+#     print(" u cant vote");
+
+# n=int(input("enter number to finds day= "));
+# if n == 1:
+#     print("its sunday");
+# elif n==2:
+#     print("its monday");
+# elif n==3:
+#     print("its tuesday");
+# elif n==4:
+#     print("its wednesdayy");
+# elif n==5:
+#     print("its thursday");
+# elif n==6:
+#     print("its friday");
+# elif n==7:
+#     print("its saturdat");
+# else :
+#     print("ERROR number must be (1-7)")
+
+
